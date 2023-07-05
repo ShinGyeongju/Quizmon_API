fa6f2e9f358e57aa532b0e537104b2bd5c119cc8
ShinGyeongju <sinkj98@naver.com>
2023-07-05 16:35:58 +0900

    CORS 허용 기능 수정

--- src/main/java/kr/quizmon/api/global/config/SecurityConfig.java
+++ src/main/java/kr/quizmon/api/global/config/SecurityConfig.java
@@ -41,7 +41,6 @@ public class SecurityConfig {
         configuration.addAllowedOrigin("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
-        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
@@ -63,7 +62,7 @@ public class SecurityConfig {
                 .sessionManagement((sessionManagement) -> sessionManagement
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .addFilterBefore(new HmacAuthenticationFilter(customConfig), UsernamePasswordAuthenticationFilter.class)
+                //.addFilterBefore(new HmacAuthenticationFilter(customConfig), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new JwtAuthenticationFilter(jwtProvider, redisIO), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
